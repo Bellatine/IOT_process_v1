650e3fbb2fad546f218e856e0fec1f3f68069861
namhhbg0001 <nam.ng205103@sis.hust.edu.vn>
2024-06-24 21:52:45 +0700

    fix pool controller

--- src/main/java/com/namng/iot_process_v1/controller/PoolController.java
+++ src/main/java/com/namng/iot_process_v1/controller/PoolController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/iot/device")
+@RequestMapping("/api/iot/pool")
 public class PoolController {
 
     private static final Logger logger = LogManager.getLogger(PoolController.class);
