f64c9f20fd071f46bb7245d97d14b1275364f226
namhhbg0001 <nam.ng205103@sis.hust.edu.vn>
2024-06-24 21:27:30 +0700

    fix log

--- src/main/java/com/namng/iot_process_v1/repository/LogDeviceRepository.java
+++ src/main/java/com/namng/iot_process_v1/repository/LogDeviceRepository.java
@@ -13,11 +13,11 @@ import java.util.List;
 @Repository
 public interface LogDeviceRepository extends JpaRepository<LogDevice, Long> {
 
-    @Query("select l from LogDevice l where l.status_app = :status_app")
-    List<LogDevice> findWithStatusApp(@Param("status_app") Long status_app);
+    @Query("select l from LogDevice l where l.status_app = :status_app and l.device_id = :device_id")
+    List<LogDevice> findWithStatusApp(@Param("status_app") Long status_app, @Param("device_id") Long device_id);
 
-    @Query("select l from LogDevice l where l.status_web = :status_web")
-    List<LogDevice> findWithStatusWeb(@Param("status_web") Long status_web);
+    @Query("select l from LogDevice l where l.status_web = :status_web and l.device_id = :device_id")
+    List<LogDevice> findWithStatusWeb(@Param("status_web") Long status_web, @Param("device_id") Long device_id);
 
     @Modifying
     @Transactional
